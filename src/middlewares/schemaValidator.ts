904667962f88298a86e843bb071069640935d2b1
Isadora Ávila <isadorgravila@gmail.com>
2022-09-07 16:47:42 -0300

    set express and database configs

---
+++ src/middlewares/schemaValidator.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function validateSchema(schema: any) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            throw {code: 'schema_error', message: error.details.map((detail: { message: any }) => detail.message)}
+        }
+
+        next();
+    }
+}
