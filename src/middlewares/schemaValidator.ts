2431cc537191fe6c8d45cf3924831a3fd748ed6f
Isadora Ávila <isadorgravila@gmail.com>
2022-09-12 12:14:58 -0300

    schemaValidator - abortEarly: false

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export default function validateSchema(schema: any) {
     return (req: Request, res: Response, next: NextFunction) => {
-        const { error } = schema.validate(req.body);
+        const { error } = schema.validate(req.body, {abortEarly: false});
         if (error) {
             throw {code: 'schema_error', message: error.details.map((detail: { message: any }) => detail.message)}
         }
