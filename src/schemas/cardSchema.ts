109159bfe3751ef8d3895ba36f9d20ccd4367852
Isadora Ávila <isadorgravila@gmail.com>
2022-09-11 21:27:33 -0300

    fix card schema

--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -3,11 +3,11 @@ import { ICardSchema } from '../types/cardTypes';
 
 const cardSchema = joi.object<ICardSchema>({
     title: joi.string().required(),
-    number: joi.string().required(),
+    number: joi.string().length(16).pattern(/^[0-9]+$/).required(),
     cardHolderName: joi.string().required(),
-    securityCode: joi.string().length(3).required(),
-    expirationDate: joi.string().required(),
-    password: joi.string().required(),
+    securityCode: joi.string().length(3).pattern(/^[0-9]+$/).required(),
+    expirationDate: joi.string().pattern(/^(0[1-9]|1[012])[-\/.]([0-9][0-9])$/).required(),
+    password: joi.string().pattern(/^[0-9]+$/).required(),
     isVirtual: joi.boolean().strict().required(),
     type: joi.string().valid('credit', 'debit', 'both').required()
   });
